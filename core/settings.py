9ef615009c243a7eb1ea10bb3a27e3cff6072425
Sidhartgautam <developer.siddharthagautam@gmail.com>
2024-11-25 10:51:52 +0545

    allowed host

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-f=#yjoxo&07wg&_ly&v)lkvumfk6d_fti+cgtlm2o3&fn7d!m9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 SHARED_APPS = [
