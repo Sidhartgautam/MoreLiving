65baa1986a955739a9d87e730d3bf352d60819da
Sidhartgautam <developer.siddharthagautam@gmail.com>
2024-12-11 23:39:27 +0545

    whitenoise added

--- core/settings.py
+++ core/settings.py
@@ -137,6 +137,7 @@ SSO_SERVICE_URL = 'https://moretrek.com/api/'
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
